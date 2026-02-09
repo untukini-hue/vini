e392f41fafa317cd31de68690d912368968d4a20
untukini-hue <untukini18@gmail.com>
2026-02-09 23:07:53 +0700

    penambahan login dan design

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-function tes() {
-  container = document.getElementById("container")  
-  texts = document.getElementsByClassName("text")
-  for (t of texts) {
-    container.appendChild(t)
-    t.classList.add("show")
-    
-    console.log(1)
+function run(){
+  document.getElementById("topLeft").classList.add("topLeft")
+  document.getElementById("topRight").classList.add("topRight")
+  document.getElementById("bottomLeft").classList.add("bottomLeft")
+  document.getElementById("bottomRight").classList.add("bottomRight")
+}
+
+function prosesLogin(){
+  Username = document.getElementById("Username").value
+  Password = document.getElementById("Password").value
+
+  if (Username == "wajib ini?" && Password == "12612961"){
+    document.body.classList.add("get-it")
+    document.getElementById("pertanyaan").classList.add("get-per")
+    document.getElementById("h1").innerHTML = `Ohh siap siap bos`
+    document.getElementById("pernyataan").classList.add("get-par")
   }
 }
